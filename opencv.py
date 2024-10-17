b720e752921a0d75c4cdb276f468517b512745ad
hydrogenbond001 <119727587+hydrogenbond001@users.noreply.github.com>
2024-10-17 19:40:59 +0800

    q

--- opencv.py
+++ opencv.py
@@ -4,14 +4,17 @@ import serial
 import time
 
 # 初始化串口通信（根据实际的串口号和波特率设置）
-ser = serial.Serial('COM6', 9600, timeout=1)  # 如果在Linux上，可能是 '/dev/ttyUSB0'
+# ser = serial.Serial('COM6', 9600, timeout=1)  # 如果在Linux上，可能是 '/dev/ttyUSB0'
 time.sleep(2)  # 给串口一些初始化时间
 
 # 初始化摄像头
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 
 # 设置字体
 font = cv2.FONT_HERSHEY_SIMPLEX
+
+# Right HSV: H=58.84, S=13.42, V=169.29
+# Left HSV: H=34.30, S=16.12, V=174.51
 
 # 定义 HSV 范围用于颜色分割（这里以左右颜色为例）
 # 左边颜色范围（根据实际图像调整）
@@ -27,9 +30,10 @@ kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (8, 8))
 
 while True:
     # 读取摄像头帧
-    ret, frame = cap.read()
-    if not ret:
-        break
+    # ret, frame = cap.read()
+    frame=cv2.imread("C:/Users/L3101/Pictures/Camera Roll/WIN_20241015_23_28_15_Pro.jpg")
+    # if not ret:
+    #     break
     height, width = frame.shape[:2]
     x1, y1, x2, y2 = width // 4, height // 4, 3 * width // 4, 3 * height // 4
 
@@ -59,32 +63,35 @@ while True:
     edges = cv2.Canny(roi, 50, 150, apertureSize=3)
 
     # 霍夫变换检测直线
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, threshold=150)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=220,minLineLength=100,maxLineGap=100)
 
     # 绘制检测到的直线并计算斜率
     if lines is not None:
-        for rho, theta in lines[:, 0]:
+        # for rho, theta in lines[:, 0]:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]  # 解包为四个点 (x1, y1, x2, y2)
+
             # 计算直线的两个点
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            x1 = int(x0 + 1000 * (-b))
-            y1 = int(y0 + 1000 * (a))
-            x2 = int(x0 - 1000 * (-b))
-            y2 = int(y0 - 1000 * (a))
+            # a = np.cos(theta)
+            # b = np.sin(theta)
+            # x0 = a * rho
+            # y0 = b * rho
+            # x1 = int(x0 + 1000 * (-b))
+            # y1 = int(y0 + 1000 * (a))
+            # x2 = int(x0 - 1000 * (-b))
+            # y2 = int(y0 - 1000 * (a))
 
             # 计算斜率
             if x2 - x1 != 0:
                 slope = 100*(y2 - y1) / (x2 - x1)
                 print(f"{(x1/2+x2/2):.0f}",end=' ')
                 print(f"{(y1/2+y2/2):.0f}",end=' ')
-                print(f"{slope:.2f}")
+                print(f"{slope:.0f}")
 
                 # 将斜率发送到串口
-                ser.write(b'\x03')
-                ser.write(f'{slope:.0f}'.encode())
-                ser.write(b'\xFE')
+                # ser.write(b'\x03')
+                # ser.write(f'{slope:.0f}'.encode())
+                # ser.write(b'\xFE')
 
             # 在原图上绘制直线
             cv2.line(roi, (x1, y1), (x2, y2), (0, 255, 0), 2)
@@ -99,10 +106,10 @@ while True:
         break
 
 # 关闭串口
-ser.close()
+# ser.close()
 
 # 释放摄像头和关闭窗口
-cap.release()
+# cap.release()
 cv2.destroyAllWindows()
 
 
